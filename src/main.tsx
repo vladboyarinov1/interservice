1ea6f0244adc7ac1faf5fd40e123a3be13d3b2d6
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-11-25 17:37:02 +0300

    feat: add styles and images

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import App from '@/App'
 import { createRoot } from 'react-dom/client'
 
+import '@fontsource-variable/montserrat'
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
